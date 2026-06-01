7985d2db0fc428a2438c7446fc301ed0690ab2a3
Evandro Lopes Barbosa <evandrolopes1234extreme@gmail.com>
2026-06-01 06:46:12 -0300

    chore: initial commit com dependencias do spring boot

---
+++ src/test/java/com/ifba/academia/SistemaAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifba.academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
