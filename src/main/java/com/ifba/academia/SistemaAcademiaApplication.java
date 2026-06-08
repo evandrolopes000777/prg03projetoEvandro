5507b501c02f0fbd07b25d78c8814dd76859bcd3
Evandro Lopes Barbosa <evandrolopes1234extreme@gmail.com>
2026-06-07 23:51:36 -0300

    feat: conexão com Supabase

--- src/main/java/com/ifba/academia/SistemaAcademiaApplication.java
+++ src/main/java/com/ifba/academia/SistemaAcademiaApplication.java
@@ -2,12 +2,33 @@ package com.ifba.academia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SistemaAcademiaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaAcademiaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        
+        System.setProperty("java.awt.headless", "false");
+        
+        ApplicationContext context = SpringApplication.run(SistemaAcademiaApplication.class, args);
+        
+        com.ifba.academia.service.AlunoService alunoService = context.getBean(com.ifba.academia.service.AlunoService.class);
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            System.err.println("Erro ao aplicar o tema visual.");
+        }
+        
+        //Abre o JFrame
+        java.awt.EventQueue.invokeLater(() -> {
+            new com.ifba.academia.view.Dashboard(alunoService).setVisible(true);
+        });
+    }
 }
