2390ffda7ba2c15d75b227cd6b67b621e229f135
Evandro Lopes Barbosa <evandrolopes1234extreme@gmail.com>
2026-07-19 16:10:01 -0300

    fix: correcao do pacote de testes

---
+++ src/test/java/br/com/ifba/academia/SistemaAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifba.academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = AcademiaApplication.class)
+class AcademiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
